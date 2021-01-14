5a0154d36233f16af6ce6a470e88a9940b67d30c
OranM <oranmon@gmail.com>
2021-01-14 16:47:55 +0200

    Add copyright

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+
+    public currentYear:number;
 
   ngOnInit(): void {
+      this.currentYear=new Date().getFullYear();
   }
 
 }
